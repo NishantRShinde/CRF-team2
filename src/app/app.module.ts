299bcd36bc8125f63a5986988255b35deb21bb53
Madan Banda <b.madankumar0@gmail.com>
2023-05-09 00:42:09 +0530

    generated report page and added header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DataSummaryComponent } from './data-summary/data-summary.component';
 import { DatasetSelectorComponent } from './dataset-selector/dataset-selector.component';
 import { BrandbarComponent } from './brandbar/brandbar.component';
+import { ReportPageComponent } from './report-page/report-page.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     DataSummaryComponent,
     DatasetSelectorComponent,
-    BrandbarComponent
+    BrandbarComponent,
+    ReportPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
